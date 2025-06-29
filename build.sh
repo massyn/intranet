907deacab394901c5cffd8677096c6ed31d7497c
Phil Massyn <phil.massyn@icloud.com>
2025-06-29 20:54:58 +1000

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt --break-system-packages.
 python compile.py
 
 chown -R phil:www-data /usr/share/nginx/html/
+chown -R phil:www-data /etc/nginx/nginx.conf
+service nginx restart
